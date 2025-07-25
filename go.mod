dae44f7c44a8bfc3a1e9d2dc0a973e98175c5dfd
tardhiansyah <tardhians@gmail.com>
2025-07-25 16:55:48 +0700

    update say hello

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/tardhiansyah/go-say-hello
+module github.com/tardhiansyah/go-say-hello/v2
 
 go 1.24.5
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package gosayhello
 
-func SayHello() string {
-	return "Hello, World and Beyond!"
+func SayHello(name string) string {
+	return "Hello, " + name + "!"
 }
